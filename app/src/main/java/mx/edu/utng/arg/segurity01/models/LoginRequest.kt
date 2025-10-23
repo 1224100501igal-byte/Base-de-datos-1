db43122ec3078a2a65675d7e2039c3ce8cc927a2
Rafael <1224100501@alumnos.utng.edu.mx>
2025-10-23 14:37:57 -0600

    Version 1

---
+++ app/src/main/java/mx/edu/utng/arg/segurity01/models/LoginRequest.kt
@@ -0,0 +1,10 @@
+package mx.edu.utng.database.arg.segurity01.models
+
+/**
+ * Datos necesarios para hacer login
+ * Esta información se enviará al servidor
+ */
+data class LoginRequest(
+    val email: String,
+    val password: String
+)
